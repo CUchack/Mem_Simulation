eba7713f7bb17164a8c1423fe72c47c21d2257d9
Chris Hack <christopher.hack@colorado.edu>
2015-04-15 19:31:20 -0600

    Created program structure and config file for testing

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <getopt.h>
 #include <strings.h>
 #include <limits.h>
+#include <stdbool.h>
 
 // Always use a 64-bit variable to hold memory addresses
 typedef unsigned long long int mem_addr_t;
@@ -58,12 +59,14 @@ typedef struct
 }mem_params;
 
 //Initialize structure that holds default memory parameters
-mem_params Default_params = {.L1.block_size = 32, .L1.cache_size = 8192, .L1.assoc = 1,
+const mem_params Default_params = {.L1.block_size = 32, .L1.cache_size = 8192, .L1.assoc = 1,
 .L1.hit_time = 1, .L1.miss_time = 1, .L2.block_size = 64, .L2.cache_size = 32768,
 .L2.assoc = 1, .L2.hit_time = 5, .L2.miss_time = 7, .L2.transfer_time = 5,
 .L2.bus_width = 16, .mmem.sendAddr = 10, .mmem.ready = 30, .mmem.chunkTime = 15,
 .mmem.chunkSize = 8};
 
+
+mem_params Parameters;
 
 int verbosity;
 
@@ -82,18 +85,59 @@ void printSummary( int hit_count, int miss_count, int eviction_count )
    printf( "hits: %d   misses: %d   evictions: %d\n", hit_count, miss_count, eviction_count );
 }
 
+void simulate (mem_params params){
+
+    printf("L1 Parameters:\nCache size: %d \nAssociativity: %hu\n", params.L1.cache_size,params.L1.assoc);
+    printf("L2 Parameters:\nCache size: %d \nAssociativity: %hu\n", params.L2.cache_size,params.L2.assoc);
+
+           /* TODO: Initialize a cache */
+
+   //Structure for a line
+   typedef struct
+   {
+      int valid;
+      mem_addr_t tag;
+      int timestamp;
+   } line_st;
+
+   //Structure for a set; a pointer to an array of lines
+   typedef struct
+   {
+      line_st *lines;
+   } cache_set;
+
+   //Structure for a cache; a pointer to an array of sets
+   typedef struct
+   {
+      cache_set *sets;
+   } cache_t;
+
+   //allocate space for sets and for lines
+   cache_t cache;
+
+//   cache.sets = malloc( par.S * sizeof ( cache_set ) );
+//   for ( int i = 0; i < par.S; i++ )
+//   {
+//      cache.sets[i].lines = malloc( sizeof ( line_st ) * par.E );
+//   }
+
+
+}
 
 
 int main(int argc, char **argv)
 {
     char *config_file;
     char c;
+    bool config = false;
+    Parameters = Default_params;
     while ( ( c = getopt( argc, argv, "f:h" ) ) != -1 )
         {
             switch ( c )
             {
             case 'f':
                 config_file = optarg;
+                config = true;
                 printf("config file = %s\n", config_file);
                 break;
             case 'h':
@@ -102,6 +146,20 @@ int main(int argc, char **argv)
                 break;
             }
         }
+    if (!config){
+        Parameters = Default_params;
+        printf("Parameters set to default\n");
+        simulate(Default_params);
+    }
+    else{
+//        int L1_Cache,L2_cache;
+//        unsigned short L1_assoc, L2_assoc;
+        FILE *configfile = fopen(config_file, "r");
+        while (fscanf( configfile, "%d %hu %d %hu", &Parameters.L1.cache_size, &Parameters.L1.assoc, &Parameters.L2.cache_size, &Parameters.L2.assoc) == 4)
+            {
+                simulate(Parameters);
+            }
+    }
 
 //    printUsage(argv);
     return 0;
