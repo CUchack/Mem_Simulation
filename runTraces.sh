2c07d207a6eefb9dc042a3f3c7c5ba54dd38fb19
Chris Hack <christopher.hack@colorado.edu>
2015-04-30 16:07:40 -0600

    First production traces

--- runTraces.sh
+++ runTraces.sh
@@ -1,26 +1,26 @@
 #!/bin/bash 
-zcat ../Large\ Traces/bzip2.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t ../Large\ Traces/bzip2.gz&
+zcat ../Large\ Traces/bzip2.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t bzip2 &
 
-zcat ../Large\ Traces/h264ref.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t ../Large\ Traces/h264ref.gz&
-
-
-zcat ../Large\ Traces/libquantum.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t ../Large\ Traces/libquantum.gz&
-
-zcat ../Large\ Traces/omnetpp.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t ../Large\ Traces/omnetpp.gz
+zcat ../Large\ Traces/h264ref.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t h264ref &
 
 
-zcat ../Large\ Traces/sjeng.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t ../Large\ Traces/sjeng.gz&
+zcat ../Large\ Traces/libquantum.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t libquantum &
+ 
+zcat ../Large\ Traces/omnetpp.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t omnetpp
 
 
-zcat ../Large\ Traces/bzip2.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t ../Large\ Traces/bzip2.gz&
-
-zcat ../Large\ Traces/h264ref.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t ../Large\ Traces/h264ref.gz&
+zcat ../Large\ Traces/sjeng.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-8way -t sjeng &
 
 
-zcat ../Large\ Traces/libquantum.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t ../Large\ Traces/libquantum.gz
+zcat ../Large\ Traces/bzip2.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t bzip2 &
 
-zcat ../Large\ Traces/omnetpp.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t ../Large\ Traces/omnetpp.gz&
+zcat ../Large\ Traces/h264ref.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t h264ref &
 
 
-zcat ../Large\ Traces/sjeng.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t ../Large\ Traces/sjeng.gz
+zcat ../Large\ Traces/libquantum.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t libquantum
+
+zcat ../Large\ Traces/omnetpp.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t omnetpp &
+
+
+zcat ../Large\ Traces/sjeng.gz | bin/Debug/./Memory\ Simulator -f config_file-L1-small -t sjeng
 
