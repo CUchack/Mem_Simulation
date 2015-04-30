6009d6151e338e4add93bb879d94442a61a3ea9f
Chris Hack <christopher.hack@colorado.edu>
2015-04-30 13:05:36 -0600

    5 m traces

---
+++ run5MTraces.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+bin/Debug/./Memory\ Simulator -f config_file -t ../traces-5M/bzip2 < ../traces-5M/bzip2 & 
+
+bin/Debug/./Memory\ Simulator -f config_file -t ../traces-5M/h264ref < ../traces-5M/h264ref &
+
+
+bin/Debug/./Memory\ Simulator -f config_file -t ../traces-5M/libquantum < ../traces-5M/libquantum
+
+bin/Debug/./Memory\ Simulator -f config_file -t ../traces-5M/omnetpp.gz < ../traces-5M/omnetpp &
+
+
+bin/Debug/./Memory\ Simulator -f config_file -t ../traces-5M/sjeng < ../traces-5M/sjeng &
+
+
+
